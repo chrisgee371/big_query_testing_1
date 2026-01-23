6879963bfbef8b56ed60485b05092a6e35b8ee01
gee.chris@prophecy.io <gee.chris@prophecy.io>
2026-01-23 12:09:40 +0000

    Remove `bronze_retail_events_generator` transformation and its logic from `bronze_pipeline_cg1` pipeline

--- pipelines/bronze_pipeline_cg1.py
+++ pipelines/bronze_pipeline_cg1.py
@@ -1,15 +1,18 @@
-Schedule = Schedule(cron = "* 0 2 * * * *", timezone = "GMT", emails = ["email@gmail.com"], enabled = False)
-SensorSchedule = SensorSchedule(enabled = False)
+Schedules = [Schedule(
+               Name = "Schedule 1", 
+               emails = ["email@gmail.com"], 
+               emailOnStart = False, 
+               emailOnFailure = False, 
+               emailOnSuccess = False, 
+               enabled = False, 
+               cron = "* 0 2 * * * *", 
+               timezone = "GMT"
+             )]
 
-with DAG(Schedule = Schedule, SensorSchedule = SensorSchedule):
+with DAG(schedules = Schedules):
     customer_orders = Task(
         task_id = "customer_orders", 
         component = "Dataset", 
         writeOptions = {"writeMode" : "overwrite"}, 
         table = {"name" : "customer_orders", "sourceType" : "Table", "sourceName" : "prophecy_field.abhinav_demos", "alias" : ""}
-    )
-    bronze_pipeline_cg1__bronze_retail_events_generator = Task(
-        task_id = "bronze_pipeline_cg1__bronze_retail_events_generator", 
-        component = "Model", 
-        modelName = "bronze_pipeline_cg1__bronze_retail_events_generator"
     )
